9fc40e14ebeb113810ad07e298ee04eed1581a73
Sara Morillon <sara.morillon.pro@gmail.com>
2022-06-09 18:38:48 +0200

    Fix socket path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 4000, proxy: { '/api': 'http://localhost:3000' } },
+  server: { port: 4000, proxy: { '/api': 'http://localhost:3000', '/socket.io': 'http://localhost:3000' } },
 })
