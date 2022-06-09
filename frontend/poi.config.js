3ad8150ed28e819717a058ce674739bfd8b14d1b
Sara Morillon <sara.morillon.pro@gmail.com>
2022-06-09 17:02:25 +0200

    Use vite instead of poi

--- frontend/poi.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  entry: 'src/index',
-  plugins: [{ resolve: '@poi/plugin-typescript', options: { configFile: 'tsconfig.build.json' } }],
-  devServer: { proxy: { '/api': 'http://localhost:3000', '/socket.io': 'http://localhost:3000' } },
-}
