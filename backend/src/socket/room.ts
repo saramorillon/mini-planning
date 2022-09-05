ca49c7c9a5ec2bac1337b119e21f5649cdcf75a8
Sara Morillon <sara.morillon.pro@gmail.com>
2022-09-05 16:56:40 +0200

    Fix room

--- backend/src/socket/room.ts
+++ backend/src/socket/room.ts
@@ -3,39 +3,47 @@ import { logger } from '../libs/logger'
 
 export type User = { name: string; observer: boolean; vote: string }
 
+const rooms: Record<string, { voting: boolean; users: Record<string, User> }> = {}
+
 export function room(socket: Socket) {
-  let voting = true
-  const users: Record<string, User> = {}
+  if (!rooms[socket.nsp.name]) {
+    rooms[socket.nsp.name] = { voting: true, users: {} }
+  }
+
+  const room = rooms[socket.nsp.name]
 
   socket.on('join', (user: Omit<User, 'vote'>) => {
-    if (!users[socket.id]) {
-      users[socket.id] = { ...user, vote: '' }
-      logger.info('user_join', { room: socket.nsp.name, user: users[socket.id] })
-      socket.nsp.emit('refresh', { voting, users })
+    if (!room.users[socket.id]) {
+      room.users[socket.id] = { ...user, vote: '' }
+      logger.info('user_join', { room: socket.nsp.name, user: room.users[socket.id] })
+      socket.nsp.emit('refresh', room)
     }
   })
 
   socket.on('vote', (vote: string) => {
-    if (users[socket.id]) {
-      users[socket.id].vote = vote
-      logger.info('user_vote', { room: socket.nsp.name, user: users[socket.id] })
-      socket.nsp.emit('refresh', { voting, users })
+    if (room.users[socket.id]) {
+      room.users[socket.id].vote = vote
+      logger.info('user_vote', { room: socket.nsp.name, user: room.users[socket.id] })
+      socket.nsp.emit('refresh', room)
     }
   })
 
   socket.on('disconnect', () => {
-    if (users[socket.id]) {
-      logger.info('user_disconnect', { room: socket.nsp.name, user: users[socket.id] })
-      delete users[socket.id]
-      socket.nsp.emit('refresh', { voting, users })
+    if (room.users[socket.id]) {
+      logger.info('user_disconnect', { room: socket.nsp.name, user: room.users[socket.id] })
+      delete room.users[socket.id]
+      socket.nsp.emit('refresh', room)
+    }
+    if (Object.keys(room.users).length === 0) {
+      delete rooms[socket.nsp.name]
     }
   })
 
-  socket.on('voting', (value: boolean) => {
-    voting = value
-    if (voting) {
-      Object.values(users).forEach((user) => (user.vote = ''))
+  socket.on('voting', (voting: boolean) => {
+    room.voting = voting
+    if (room.voting) {
+      Object.values(room.users).forEach((user) => (user.vote = ''))
     }
-    socket.nsp.emit('refresh', { voting, users })
+    socket.nsp.emit('refresh', room)
   })
 }
